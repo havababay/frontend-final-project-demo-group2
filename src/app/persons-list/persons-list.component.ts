2c5b2f32e390f61562491a54ac47f1d047de1695
havababay <hava.babay@gmail.com>
2024-05-30 22:52:17 +0300

    loading section

--- src/app/persons-list/persons-list.component.ts
+++ src/app/persons-list/persons-list.component.ts
@@ -8,42 +8,53 @@ import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { DeletePersonDialogComponent } from '../delete-person-dialog/delete-person-dialog.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-persons-list',
   standalone: true,
   imports: [
-    CommonModule, MatTableModule, MatIconModule, MatButtonModule, RouterModule
+    CommonModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule,
+    MatProgressBarModule,
   ],
   templateUrl: './persons-list.component.html',
   styleUrl: './persons-list.component.css',
 })
-export class PersonsListComponent implements OnInit { 
+export class PersonsListComponent implements OnInit {
+  isLoadingDone = false;
+
   constructor(
-    private personService : PeronsService, 
-    private dialogService : MatDialog) {}
-  
+    private personService: PeronsService,
+    private dialogService: MatDialog
+  ) {}
+
   ngOnInit(): void {
-    this.personService.list().then(
-      (result : Person[]) => this.allPersons = result
-    );
+    this.personService.list().then((result: Person[]) => {
+      this.allPersons = result;
+      this.isLoadingDone = true;
+    });
   }
 
-  allPersons : Person[] = [];
+  allPersons: Person[] = [];
   displayedColumns = ['fullName', 'email', 'actions'];
 
-  deletePerson(id: string, fullName : string) {
-    const dialogRef = this.dialogService.open(DeletePersonDialogComponent,{data: fullName,});
+  deletePerson(id: string, fullName: string) {
+    const dialogRef = this.dialogService.open(DeletePersonDialogComponent, {
+      data: fullName,
+    });
 
-    dialogRef.afterClosed().subscribe(deletionResult => {
-        if (deletionResult) {
-          this.personService.delete(id).then(()=>{
-            this.personService.list().then(
-              (result : Person[]) => this.allPersons = result
-            );
-          })
-        }
+    dialogRef.afterClosed().subscribe((deletionResult) => {
+      if (deletionResult) {
+        this.personService.delete(id).then(() => {
+          this.personService
+            .list()
+            .then((result: Person[]) => (this.allPersons = result));
+        });
       }
-    );
+    });
   }
 }
